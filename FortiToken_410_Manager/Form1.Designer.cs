2a56ad01051cf62c16431268d42229d7552504fb
sakura-825 <sakura.s@hypersecu.com>
2025-09-25 09:32:56 -0700

    set a resizing option to false

--- FortiToken_410_Manager/Form1.Designer.cs
+++ FortiToken_410_Manager/Form1.Designer.cs
@@ -54,12 +54,12 @@
             tableLayoutPanel1.Location = new Point(5, 5);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 5;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 13.6563873F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 11.4537449F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.9779739F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 21.1453743F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 38.546257F));
-            tableLayoutPanel1.Size = new Size(748, 454);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 12.6564674F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10.5702362F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 12.5173855F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 18.3588314F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 45.7579956F));
+            tableLayoutPanel1.Size = new Size(998, 719);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // HOTPStatusLabel
@@ -67,7 +67,7 @@
             HOTPStatusLabel.Anchor = AnchorStyles.Left;
             HOTPStatusLabel.AutoSize = true;
             HOTPStatusLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            HOTPStatusLabel.Location = new Point(3, 129);
+            HOTPStatusLabel.Location = new Point(3, 193);
             HOTPStatusLabel.Name = "HOTPStatusLabel";
             HOTPStatusLabel.Size = new Size(103, 37);
             HOTPStatusLabel.TabIndex = 3;
@@ -81,19 +81,19 @@
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 70.21564F));
             tableLayoutPanel2.Controls.Add(button2, 1, 0);
             tableLayoutPanel2.Controls.Add(button1, 0, 0);
-            tableLayoutPanel2.Location = new Point(3, 185);
+            tableLayoutPanel2.Location = new Point(3, 261);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 1;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel2.Size = new Size(742, 90);
+            tableLayoutPanel2.Size = new Size(992, 124);
             tableLayoutPanel2.TabIndex = 0;
             // 
             // button2
             // 
             button2.Anchor = AnchorStyles.Left;
             button2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button2.Location = new Point(224, 20);
+            button2.Location = new Point(298, 37);
             button2.Name = "button2";
             button2.Size = new Size(176, 49);
             button2.TabIndex = 1;
@@ -104,7 +104,7 @@
             // 
             button1.Anchor = AnchorStyles.Left;
             button1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(3, 20);
+            button1.Location = new Point(3, 37);
             button1.Name = "button1";
             button1.Size = new Size(176, 49);
             button1.TabIndex = 0;
@@ -116,7 +116,7 @@
             Title.Anchor = AnchorStyles.Left;
             Title.AutoSize = true;
             Title.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            Title.Location = new Point(3, 12);
+            Title.Location = new Point(3, 27);
             Title.Name = "Title";
             Title.Size = new Size(333, 37);
             Title.TabIndex = 1;
@@ -127,7 +127,7 @@
             keyStateLabel.Anchor = AnchorStyles.Left;
             keyStateLabel.AutoSize = true;
             keyStateLabel.Font = new Font("Segoe UI", 12F);
-            keyStateLabel.Location = new Point(3, 77);
+            keyStateLabel.Location = new Point(3, 118);
             keyStateLabel.Name = "keyStateLabel";
             keyStateLabel.Size = new Size(260, 21);
             keyStateLabel.TabIndex = 2;
@@ -141,17 +141,21 @@
             listBox1.ForeColor = Color.Red;
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(3, 281);
+            listBox1.Location = new Point(3, 392);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(742, 165);
+            listBox1.Size = new Size(992, 315);
             listBox1.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(758, 464);
+            ClientSize = new Size(1008, 729);
             Controls.Add(tableLayoutPanel1);
+            MaximizeBox = false;
+            MaximumSize = new Size(1024, 768);
+            MinimizeBox = false;
+            MinimumSize = new Size(1024, 768);
             Name = "Form1";
             Text = "FortiToken 410 Manager";
             tableLayoutPanel1.ResumeLayout(false);
